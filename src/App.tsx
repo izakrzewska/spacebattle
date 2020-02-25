e3310fc5616d6f9a923c0d6d4144353c90d1c2ee
Iga Zakrzewska <igazakrzewska@MacBook-Air-Iga.local>
2020-02-25 23:33:17 +0100

    get people and starship counts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [peopleCount, setPeopleCount] = useState<number>(0);
+  const [starshipsCount, setStarshipsCount] = useState<number>(0);
+  const [hasErrors, setHasErrors] = useState<boolean>(false);
+
+  const fetchPeople = () => {
+    fetch("https://swapi.co/api/people")
+      .then(res => res.json())
+      .then(res => setPeopleCount(res.count))
+      .catch(() => setHasErrors(true));
+  };
+
+  const fetchStarships = () => {
+    fetch("https://swapi.co/api/starships")
+      .then(res => res.json())
+      .then(res => setStarshipsCount(res.count))
+      .catch(() => setHasErrors(true));
+  };
+
+  useEffect(() => {
+    async function getPeopleData() {
+      await fetchPeople();
+    }
+
+    async function getStarshipsData() {
+      await fetchStarships();
+    }
+
+    getPeopleData();
+    getStarshipsData();
+  }, []);
+
+  !!peopleCount && console.log(peopleCount);
+  !!starshipsCount && console.log(starshipsCount);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {hasErrors ? <div>cos poszlo nie tak</div> : <div>tu bedzie apka</div>}
     </div>
   );
 }
